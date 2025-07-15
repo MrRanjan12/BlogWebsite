2af2b88802466d6d3e030c7932d990708da05aa9
MrRanjan12 <ranjankumar4447758@gmail.com>
2025-07-15 21:14:42 +0530

    Updated for uploading on production

--- Frontend/src/api/authApi.js
+++ Frontend/src/api/authApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://blogwebsite-73y4.onrender.com/" });
 
 export const registerUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
--- Frontend/src/api/blogApi.js
+++ Frontend/src/api/blogApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://blogwebsite-73y4.onrender.com/" });
 
 // Send token if exists
 API.interceptors.request.use((req) => {
